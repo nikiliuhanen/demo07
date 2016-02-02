2adc2b4e55029f79c6ff6508080668acd8fb692e
nikiliuhanen <niki.liuhanen@gmail.com>
2016-02-02 11:08:42 +0200

    demo07

---
+++ Demo07/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo07
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string line = null;
+            StreamWriter outputFile = new StreamWriter(@"d:\T1Lines.txt");
+            
+            
+            {
+                // kysytään käyttäjältä rivi
+                do
+                {
+
+                    Console.WriteLine("Give a text line (enter stops):");
+                    line = Console.ReadLine();
+                    outputFile.WriteLine(line); // kirjoitetaan levylle
+                }
+                while (line.Length != 0);
+                }
+            // suljetaan tiedosto
+            outputFile.Close();
+
+            // avaa tiedosto
+            try
+            {
+                using (StreamReader sr = new StreamReader(@"d:\T1Lines.txt"))
+                {
+                    // read the stream to a string, and write the string to console
+                    String line2 = sr.ReadToEnd();
+                    Console.WriteLine(line2);
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("The file could not be read:");
+                Console.WriteLine(e.Message);
+            }
+            }
+        }
+        }
+    
+
---
+++ tehtävä2/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace tehtävä2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string number = null;
+
+           
+            {
+                // kysytään käyttäjältä rivi
+                do
+                {
+                    StreamWriter outputFile = new StreamWriter(@"d:\integer.txt");
+                    Console.WriteLine("Give a number (enter stops):");
+                    int integer;
+                    
+                    bool parsed = Int32.TryParse(number, out integer);
+                    if (!parsed)
+
+                        number = Console.ReadLine();
+
+                    outputFile.WriteLine(integer); // kirjoitetaan levylle
+                
+                else
+                {
+                    double doublerino;
+                    bool parsed2 = Double.TryParse(number, out doublerino);
+                        StreamWriter outputFile2 = new StreamWriter(@"d:\double.txt");
+
+                        {
+                            while (number.Length != 0) ;
+                        }
+                    }
+
+                    // suljetaan tiedosto
+                    outputFile.Close();
+
+                
+    
+
